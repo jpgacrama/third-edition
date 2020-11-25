c933e834f586b1dbe8e2ebcae8db299cd80f533e
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2020-11-25 10:53:44 +0800

    Cleaned the code using Code Maid

--- VS2013/Chapter_4/SloppyJoe/SloppyJoe/MenuMaker.cs
+++ VS2013/Chapter_4/SloppyJoe/SloppyJoe/MenuMaker.cs
@@ -1,19 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SloppyJoe
 {
-    class MenuMaker
+    internal class MenuMaker
     {
         public Random Randomizer;
 
-        string[] Meats = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami" };
-        string[] Condiments = { "yellow mustard", "brown mustard",
+        private string[] Meats = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami" };
+
+        private string[] Condiments = { "yellow mustard", "brown mustard",
                    "honey mustard", "mayo", "relish", "french dressing" };
-        string[] Breads = { "rye", "white", "wheat", "pumpernickel",
+
+        private string[] Breads = { "rye", "white", "wheat", "pumpernickel",
                    "italian bread", "a roll" };
 
         public string GetMenuItem()
@@ -23,6 +21,5 @@ namespace SloppyJoe
             string randomBread = Breads[Randomizer.Next(Breads.Length)];
             return randomMeat + " with " + randomCondiment + " on " + randomBread;
         }
-
     }
 }
