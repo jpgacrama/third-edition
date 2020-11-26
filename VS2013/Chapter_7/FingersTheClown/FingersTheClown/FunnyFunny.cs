81e3437ca899408d4c76f6ff068334352314886b
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2020-11-26 15:59:03 +0800

    Cleaned the Code using Code Maid

--- VS2013/Chapter_7/FingersTheClown/FingersTheClown/FunnyFunny.cs
+++ VS2013/Chapter_7/FingersTheClown/FingersTheClown/FunnyFunny.cs
@@ -1,22 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FingersTheClown
 {
-    class FunnyFunny : IClown
+    internal class FunnyFunny : IClown
     {
         public FunnyFunny(string funnyThingIHave)
         {
             this.funnyThingIHave = funnyThingIHave;
         }
+
         private string funnyThingIHave;
+
         public string FunnyThingIHave
         {
             get { return "Hi kids! I have " + funnyThingIHave; }
         }
+
         public void Honk()
         {
             Console.WriteLine(this.FunnyThingIHave);
